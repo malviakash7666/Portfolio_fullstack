f8fbe8b907f562eab39bc1fb9fb8a41127b30286
Akash Ajay Malvi <akashmalvi@gmail.com>
2026-03-22 18:32:05 +0530

    update deploy 2

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,16 +1,19 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-  }
-);
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, { dialect: "postgres", protocol: "postgres", logging: false })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
+        logging: false,
+      }
+    );
 
 export default sequelize;
